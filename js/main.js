8eb9d96ac09317b6718c90aee1ee6d0b2960a963
Priya Yasasvin <priyayasasvin@Priyas-MacBook-Air.local>
2021-06-07 22:36:00 +0100

    coding js code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-let UserLoggedIn = true;
-if (UserLoggedIn) {
-    console.log('User logged in');
-}
-else {
-   console.log('Alert'); 
-}
+fetch("http://localhost:3000/api/cameras").then(response => response.json()).then(Cameras => {
+    console.log('results:', Cameras)
+})
+    .catch(error => { console.error(error) })
+
+var Camera = [camera0, camera1, camera2, camera3, camera4];
+const Camera= document.getElementById("CameraList");
+document.getElementById("VCam");
+var first = camera[0];
+const VCam = document.createElement("img");
+VCam.src = "../image/vcam_1.jpg";
+VCam.width = 300;
+VCam.height = 300;
+VCam.appendChild(Camera);
+
+
+
