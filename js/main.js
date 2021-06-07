ff7e5d4ad78045ea71cf4b7c350fbbb225e73828
Priya Yasasvin <priyayasasvin@Priyas-MacBook-Air.local>
2021-06-07 22:48:57 +0100

    create Camera on js

--- js/main.js
+++ js/main.js
@@ -14,4 +14,14 @@ VCam.height = 300;
 VCam.appendChild(Camera);
 
 
+document.getElementsByClassName("description");
+const description = document.createElement("h4");
+description.textContent = "The PRONTOR ZEISS-IKON COLOR-PANTAR 1:2,8/45"
+const description = document.createElement("p");
+description.textContent = "Introduced in the mid 1950s, this was a simple 35mm camera with superb Zeiss build quality.This example is the slightly later version introduced in 1958It is in very good condition with shutter, metering and film advance working correctly and smooth focus operation. The optics are fungus free"
+VCam.appendChild(CameraList);
 
+
+const option = document.createElement(span);
+document.getElementsByClassName("option");
+option.src = "fa fa-shopping-basket"
