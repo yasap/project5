69c46f08326aa5aec1d5808ea87ad331205fa344
Priya Yasasvin <priyayasasvin@Priyas-MacBook-Air.local>
2021-07-10 20:05:48 +0100

    completed detail page

--- js/detail.js
+++ js/detail.js
@@ -5,37 +5,29 @@ let cameraid = params.get("id");
 fetch("http://localhost:3000/api/cameras/" + cameraid).then(response => response.json()).then(Camera => {
     console.log('results:', Camera)
 
-    var image = document.createElement("img");
-    image.src = Camera.imageUrl;
-    document.body.appendChild(image);
-
-
  const detailcontainer = document.getElementById("detaillist");
-        var det = document.createElement("Div");
+    var det = document.createElement("Div");
     const detailimage = document.createElement("img");
-        detailimage.src = cameraid.imageUrl;
-        detailimage.alt = cameraid.name;
-        detailimage.style.width = "50px";
-        detailimage.style.height = "10px";
+    detailimage.classList.add("Detailimage");
+        detailimage.src = Camera.imageUrl;
+    detailimage.alt = Camera.name;
     det.appendChild(detailimage);
+     detailcontainer.appendChild(det);
         const description = document.createElement("div");
     description.classList.add("descriptions");
-    var title = document.createElement("H4");
-        title.textContent = cameraid.name;
-    description.appendChild(title);
-    var text = document.createElement("p");
-        text.textContent = cameraid.description;
-        description.appendChild(text);
-    description.appendChild(title);
-    var icon = document.createElement("div");
-        icon.classList.add("icons");
+    var dettitle = document.createElement("H4");
+        dettitle.textContent = Camera.name;
+    description.appendChild(dettitle);
+    var dettext = document.createElement("p");
+        dettext.textContent = Camera.description;
+    description.appendChild(dettext);
+    var price = document.createElement("H3");
+        price.textContent = ("£" + Camera.price);
+    description.appendChild(price);
         var basketicon = document.createElement("span");
         basketicon.classList.add("fa");
         basketicon.classList.add("fa-shopping-basket");
-        var detaillink = document.createElement("a");
-        detaillink.href = "detail.html?id=" + camera._id;
-        icons.appendChild(basketicon);
-        icons.appendChild(detaillink);
-        vcam.appendChild(icons);
+    description.appendChild(basketicon);
+    detailcontainer.appendChild(description);
     })
 
