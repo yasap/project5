4121e05ef43de945fb8a35f54442e24464a2034e
Priya Yasasvin <priyayasasvin@Priyas-MacBook-Air.local>
2021-07-14 20:18:06 +0100

    add lenses to detail.js

--- js/detail.js
+++ js/detail.js
@@ -5,29 +5,38 @@ let cameraid = params.get("id");
 fetch("http://localhost:3000/api/cameras/" + cameraid).then(response => response.json()).then(Camera => {
     console.log('results:', Camera)
 
- const detailcontainer = document.getElementById("detaillist");
+    const detailcontainer = document.getElementById("detaillist");
     var det = document.createElement("Div");
     const detailimage = document.createElement("img");
     detailimage.classList.add("Detailimage");
-        detailimage.src = Camera.imageUrl;
+    detailimage.src = Camera.imageUrl;
     detailimage.alt = Camera.name;
     det.appendChild(detailimage);
-     detailcontainer.appendChild(det);
-        const description = document.createElement("div");
+    detailcontainer.appendChild(det);
+    const description = document.createElement("div");
     description.classList.add("descriptions");
     var dettitle = document.createElement("H4");
-        dettitle.textContent = Camera.name;
+    dettitle.textContent = Camera.name;
     description.appendChild(dettitle);
+    let ul = document.createElement("ul");
+    Camera.lenses.forEach(lense => {
+        let li = document.createElement("li");
+        li.textContent = lense;
+        ul.appendChild(li);
+    });
+    description.appendChild(ul);
     var dettext = document.createElement("p");
-        dettext.textContent = Camera.description;
+    dettext.textContent = Camera.description;
     description.appendChild(dettext);
     var price = document.createElement("H3");
-        price.textContent = ("£" + Camera.price);
+    price.textContent = ("£" + Camera.price);
     description.appendChild(price);
-        var basketicon = document.createElement("span");
-        basketicon.classList.add("fa");
-        basketicon.classList.add("fa-shopping-basket");
-    description.appendChild(basketicon);
-    detailcontainer.appendChild(description);
-    })
+ 
 
+    var basketicon = document.createElement("span");
+    basketicon.classList.add("basketicon");
+        basketicon.classList.add("fa");
+    basketicon.classList.add("fa-shopping-basket");
+        description.appendChild(basketicon);
+        detailcontainer.appendChild(description);
+    })
